e5e1fda673332f606f9e5c6fe11f5e7485b39e06
Liz <jesusridesthebus@gmail.com>
2019-04-12 12:27:48 -0700

    add bowl prototype - partial

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
 function Bowl(size, beans, toppings, price) {
   this.size = size,
   this.beans = beans,
-  this.toppings = toppings,
-  this.price = function priceCalculation(price) {
-    return '$' + price.toFixed(2);
-  }
+  this.toppings = [],
+  this.price = priceCalculation(price);
 }
 
-// function priceCalculation(price) {
-//   return '$' + price.toFixed(2);
-// }
+function priceCalculation(price) {
+  return '$' + price.toFixed(2);
+}
 
 Bowl.prototype.showPrice = function() {
   $('#bowl-result').empty();
-  
+
+
+  $('#bowl-result').append('<p>Bowl size: ' + this.size + '</p>');
+  $('#bowl-result').append('<p>Beans: ' + this.beans + '</p>');
+  $('#bowl-result').append('<p>Toppings: ' + asldkfj + '</p>');
+  $('#bowl-result').append('<p>Bowl Price: ' + this.price + '</p>');
 }
