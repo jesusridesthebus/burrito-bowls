1c813c883fe654df629678cf2e27b59965ad0068
Liz <jesusridesthebus@gmail.com>
2019-04-12 14:29:03 -0700

    add UI logic to js, need to get toppings to display

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,23 @@ Bowl.prototype.showPrice = function() {
   $('#bowl-result').append('<p>Bowl size: ' + this.size + '</p>');
   $('#bowl-result').append('<p>Beans: ' + this.beans + '</p>');
   $('#bowl-result').append('<p>Meat: ' + this.meat + '</p>');
-  $('#bowl-result').append('<p>Toppings: ' + asldkfj + '</p>');
+  $('#bowl-result').append('<p>Toppings: ' + this.toppings + '</p>');
   $('#bowl-result').append('<p>Bowl Price: ' + this.price + '</p>');
 }
+
+$(function() {
+  $('form#burrito-form').submit(function(event) {
+    event.preventDefault();
+    var bowlSize = $('#size option:selected').text();
+    var beans = $('#beans option:selected').text();
+    var meat = $('#meat option:selected').text();
+    var toppingsList = $('input:checkbox[name=toppings]:checked').each(function() {
+      var toppings = $(this).text();
+      $('#bowl-result').append(toppings + ', ');
+    });
+    var bowlPrice = parseInt($('#size').val())+parseInt($('#meat').val());
+
+    var burritoBowl = new Bowl(bowlSize, beans, meat, toppingsList, bowlPrice);
+    burritoBowl.showPrice();
+  });
+});
